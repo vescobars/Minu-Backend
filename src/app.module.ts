24f559d41714b43568aa527149b854deea306f79
veronica <v.escobar1@uniandes.edu.co>
2022-09-09 09:13:12 -0500

    Se crearon los modulos y entidades para las clases de PayMode, Order, OrderDetail, Table

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OrderModule } from './order/order.module';
+import { PayModeModule } from './pay-mode/pay-mode.module';
+import { OrderDetailModule } from './order-detail/order-detail.module';
+import { TableModule } from './table/table.module';
 
 @Module({
-  imports: [],
+  imports: [OrderModule, PayModeModule, OrderDetailModule, TableModule],
   controllers: [AppController],
   providers: [AppService],
 })
