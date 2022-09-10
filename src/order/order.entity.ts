a3fdaeab157267c6c3a8ab787116650cd4b1eee4
veronica <v.escobar1@uniandes.edu.co>
2022-09-09 20:54:19 -0500

    Assigning owner of relation to one to one relations

--- src/order/order.entity.ts
+++ src/order/order.entity.ts
@@ -3,6 +3,7 @@ import { OrderDetailEntity } from 'src/order-detail/order-detail.entity';
 import { PayModeEntity } from 'src/pay-mode/pay-mode.entity';
 import { TableEntity } from 'src/table/table.entity';
 import {OneToOne,Column, Entity, PrimaryGeneratedColumn} from 'typeorm';
+import { JoinColumn } from 'typeorm/decorator/relations/JoinColumn';
 
 @Entity()
 export class OrderEntity {
@@ -19,11 +20,14 @@ export class OrderEntity {
     totalValue: number;
 
     @OneToOne(() => OrderDetailEntity, orderDetail => orderDetail.order)
+    @JoinColumn()
     orderDetail: OrderDetailEntity;
 
     @OneToOne(() => PayModeEntity, payMode => payMode.order)
+    @JoinColumn()
     payMode: PayModeEntity;
 
     @OneToOne(() => TableEntity, table => table.order)
+    @JoinColumn()
     table: TableEntity;
 }
