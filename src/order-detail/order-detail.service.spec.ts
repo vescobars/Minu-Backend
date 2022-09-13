f830637764ac301641a6c54aa2e1910d25066e77
veronica <v.escobar1@uniandes.edu.co>
2022-09-13 18:50:58 -0500

    Finished implementing the OrderDetail Service, started setup for tests for OrderDetail Service

--- src/order-detail/order-detail.service.spec.ts
+++ src/order-detail/order-detail.service.spec.ts
@@ -1,18 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { TypeOrmTestingConfig } from '../shared/testing-utils/typeorm-testing-config';
+import { OrderDetailEntity } from './order-detail.entity';
 import { OrderDetailService } from './order-detail.service';
 
 describe('OrderDetailService', () => {
-  let service: OrderDetailService;
+ let service: OrderDetailService;
+ let repository: Repository<OrderDetailEntity>;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [OrderDetailService],
-    }).compile();
+ beforeEach(async () => {
+   const module: TestingModule = await Test.createTestingModule({
+     imports: [...TypeOrmTestingConfig()],
+     providers: [OrderDetailService],
+   }).compile();
 
-    service = module.get<OrderDetailService>(OrderDetailService);
-  });
+   service = module.get<OrderDetailService>(OrderDetailService);
+   repository = module.get<Repository<OrderDetailEntity>>(getRepositoryToken(OrderDetailEntity));
+ });
+  
+ it('should be defined', () => {
+   expect(service).toBeDefined();
+ });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
 });
+
