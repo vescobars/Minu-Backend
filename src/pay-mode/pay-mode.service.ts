ab0b482f3d82ff6ab74765a1b6f681c31e736ec8
veronica <v.escobar1@uniandes.edu.co>
2022-09-13 08:36:26 -0500

    Creaded business logic error classes. Also started implementing methods for the PayMode Service

--- src/pay-mode/pay-mode.service.ts
+++ src/pay-mode/pay-mode.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { PayModeEntity } from './pay-mode.entity';
+import { BusinessError, BusinessLogicException } from '../shared/errors/business-errors';
+
 
 @Injectable()
-export class PayModeService {}
+export class PayModeService {
+
+    constructor(
+        @InjectRepository(PayModeEntity)
+        private readonly payModeRepository: Repository<PayModeEntity>
+    ){}
+    
+    async findAll(): Promise<PayModeEntity[]>{
+        return await this.payModeRepository.find();
+    }
+
+    async findOne(id: string): Promise<PayModeEntity>{
+        const paymode: PayModeEntity = await this.payModeRepository.findOne({where: {id}});
+        if(!paymode){
+            throw new BusinessLogicException("The paymode with the given id was not found", BusinessError.NOT_FOUND)
+        }
+
+        return paymode;
+    }
+
+    
+}
