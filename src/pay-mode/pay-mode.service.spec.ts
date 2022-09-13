d131c7bd560bb074c7de299c665fdbebab8ca7d5
veronica <v.escobar1@uniandes.edu.co>
2022-09-13 10:24:51 -0500

    Finished seeds for pay mode test database

--- src/pay-mode/pay-mode.service.spec.ts
+++ src/pay-mode/pay-mode.service.spec.ts
@@ -8,6 +8,8 @@ import { PayModeEntity} from './pay-mode.entity';
 describe('PayModeService', () => {
   let service: PayModeService;
   let repository: Repository<PayModeEntity>;
+  let payModeList: PayModeEntity[];
+  let payTypesList: string[];
   
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -17,8 +19,19 @@ describe('PayModeService', () => {
 
     service = module.get<PayModeService>(PayModeService);
     repository = module.get<Repository<PayModeEntity>>(getRepositoryToken(PayModeEntity));
+    await seedDatabase();
   });
 
+  const seedDatabase = async () => {
+    repository.clear();
+    payModeList = [];
+    payTypesList = ['Daviplata','PSE','Tarjeta de Credito','Tarjeta de Debito','Efectivo','Nequi','RappiPay','Coink','GooglePay','SamsungPay','ApplePay','Cheque']
+    for(let i = 0; i < 5; i++){
+        const paymode: PayModeEntity = await repository.save({
+        type: payTypesList[Math.random() * payTypesList.length]})
+        payModeList.push(paymode);
+    }
+  }
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
