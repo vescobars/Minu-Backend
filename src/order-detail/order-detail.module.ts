491505baf446afd5bdb42df3552e26a4de75c388
veronica <v.escobar1@uniandes.edu.co>
2022-09-13 18:35:31 -0500

    Initial configuration of order detail service

--- src/order-detail/order-detail.module.ts
+++ src/order-detail/order-detail.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { OrderDetailEntity } from './order-detail.entity';
 import { OrderDetailService } from './order-detail.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([OrderDetailEntity])],
   providers: [OrderDetailService]
 })
 export class OrderDetailModule {}
--- src/order-detail/order-detail.service.ts
+++ src/order-detail/order-detail.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { OrderDetailEntity } from './order-detail.entity';
 
 @Injectable()
-export class OrderDetailService {}
+export class OrderDetailService {
+
+    constructor(
+        @InjectRepository(OrderDetailEntity)
+        private readonly orderDetailRepository: Repository<OrderDetailEntity>
+    ){}
+}
