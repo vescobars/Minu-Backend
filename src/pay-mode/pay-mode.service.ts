4ed50c034837164210228f762a92c0bd0e6c2f54
veronica <v.escobar1@uniandes.edu.co>
2022-09-13 08:41:48 -0500

    All basic methods over paymode service have been implemented

--- src/pay-mode/pay-mode.service.ts
+++ src/pay-mode/pay-mode.service.ts
@@ -22,8 +22,29 @@ export class PayModeService {
         if(!paymode){
             throw new BusinessLogicException("The paymode with the given id was not found", BusinessError.NOT_FOUND)
         }
-
         return paymode;
+    }
+
+    async create(paymode: PayModeEntity): Promise<PayModeEntity>{
+        return await this.payModeRepository.save(paymode);
+    }
+
+    async update(id: string, paymode: PayModeEntity): Promise<PayModeEntity>{
+        const persistedPayMode: PayModeEntity = await this.payModeRepository.findOne({where:{id}});
+        if(!persistedPayMode){
+            throw new BusinessLogicException("The paymode with the given id doesnt exist", BusinessError.NOT_FOUND)
+        }
+
+        return await this.payModeRepository.save({...persistedPayMode,...paymode});
+    }
+
+    async delete(id: string){
+        const paymode : PayModeEntity = await this.payModeRepository.findOne({where: {id}});
+        if(!paymode){
+            throw new BusinessLogicException("The paymode with the given id doesnt exist", BusinessError.NOT_FOUND)
+        }
+
+        await this.payModeRepository.remove(paymode);
     }
 
     
