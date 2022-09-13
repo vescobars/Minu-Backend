302c41f55bbdf5f7145f203074d7eabfc989294e
veronica <v.escobar1@uniandes.edu.co>
2022-09-13 10:57:41 -0500

    Finished implementing CRUD in the table services

--- src/table/table.service.ts
+++ src/table/table.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { BusinessError, BusinessLogicException } from 'src/shared/errors/business-errors';
 import { Repository } from 'typeorm';
 import { TableEntity } from './table.entity';
 
@@ -9,4 +10,36 @@ export class TableService {
         @InjectRepository(TableEntity)
         private readonly tableRepository: Repository<TableEntity>
     ){}
+
+    async findAll(): Promise<TableEntity[]> {
+        return await this.tableRepository.find({ relations: ["order"] });
+    }
+
+    async findOne(id: string): Promise<TableEntity> {
+        const table: TableEntity = await this.tableRepository.findOne({where: {id}, relations: ["order"] } );
+        if (!table)
+          throw new BusinessLogicException("The table with the given id was not found", BusinessError.NOT_FOUND);
+   
+        return table;
+    }
+
+    async create(table: TableEntity): Promise<TableEntity> {
+        return await this.tableRepository.save(table);
+    }
+
+    async update(id: string, table: TableEntity): Promise<TableEntity> {
+        const persistedTable: TableEntity = await this.tableRepository.findOne({where:{id}});
+        if (!persistedTable)
+          throw new BusinessLogicException("The table with the given id was not found", BusinessError.NOT_FOUND);
+        
+        return await this.tableRepository.save({...persistedTable, ...table});
+    }
+
+    async delete(id: string) {
+        const table: TableEntity = await this.tableRepository.findOne({where:{id}});
+        if (!table)
+          throw new BusinessLogicException("The table with the given id was not found", BusinessError.NOT_FOUND);
+     
+        await this.tableRepository.remove(table);
+    }
 }
