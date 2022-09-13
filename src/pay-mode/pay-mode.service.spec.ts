d6fa201830eb00af2e5fbacd0776aad78828f2df
veronica <v.escobar1@uniandes.edu.co>
2022-09-13 10:07:18 -0500

    Finished setting up tests for the paymode service. Added all the entities required in the typeOrm-testing-config file

--- src/pay-mode/pay-mode.service.spec.ts
+++ src/pay-mode/pay-mode.service.spec.ts
@@ -1,15 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { TypeOrmTestingConfig } from '../shared/testing-utils/typeorm-testing-config';
 import { PayModeService } from './pay-mode.service';
+import { PayModeEntity} from './pay-mode.entity';
 
 describe('PayModeService', () => {
   let service: PayModeService;
-
+  let repository: Repository<PayModeEntity>;
+  
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [...TypeOrmTestingConfig()],
       providers: [PayModeService],
     }).compile();
 
     service = module.get<PayModeService>(PayModeService);
+    repository = module.get<Repository<PayModeEntity>>(getRepositoryToken(PayModeEntity));
   });
 
   it('should be defined', () => {
