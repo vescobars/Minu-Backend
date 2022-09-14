3f437e8efb1ed30bf703c54b1d92fb8c56286dff
veronica <v.escobar1@uniandes.edu.co>
2022-09-13 19:13:39 -0500

    Finished Seed database for order detail tests

--- src/order-detail/order-detail.service.spec.ts
+++ src/order-detail/order-detail.service.spec.ts
@@ -1,3 +1,4 @@
+import { faker } from '@faker-js/faker';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -8,6 +9,7 @@ import { OrderDetailService } from './order-detail.service';
 describe('OrderDetailService', () => {
  let service: OrderDetailService;
  let repository: Repository<OrderDetailEntity>;
+ let orderDetailList: OrderDetailEntity[];
 
  beforeEach(async () => {
    const module: TestingModule = await Test.createTestingModule({
@@ -17,7 +19,26 @@ describe('OrderDetailService', () => {
 
    service = module.get<OrderDetailService>(OrderDetailService);
    repository = module.get<Repository<OrderDetailEntity>>(getRepositoryToken(OrderDetailEntity));
- });
+   await seedDatabase();
+  });
+
+ //TODO: Preguntar en clase donde me puedo crear un metodo para auto generar filler de otras clases (donde lo pongo???)
+ const seedDatabase = async () => {
+  repository.clear();
+  orderDetailList = [];
+  for(let i = 0; i < 5; i++){
+      const orderDetail: OrderDetailEntity = await repository.save({
+      state: faker.random.word(),
+      date: faker.date.past(3),
+      totalValue: faker.datatype.number({ max: 100000 }) ,
+      orderDetail: null,
+      payMode: null,
+      table:null,
+      client:null,
+      restaurantSite:null,})
+      orderDetailList.push(orderDetail);
+  }
+}
   
  it('should be defined', () => {
    expect(service).toBeDefined();
