5412cd0feb710a5cb683ed62e7792bda3262d6fa
veronica <v.escobar1@uniandes.edu.co>
2022-09-13 19:33:23 -0500

    Finished set up for the order service

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { OrderEntity } from './order.entity';
 import { OrderService } from './order.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([OrderEntity])],
   providers: [OrderService]
 })
 export class OrderModule {}
--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { OrderEntity } from './order.entity';
 
 @Injectable()
-export class OrderService {}
+export class OrderService {
+    constructor(
+        @InjectRepository(OrderEntity)
+        private readonly orderRepository: Repository<OrderEntity>
+    ){}
+}
