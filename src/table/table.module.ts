c78229e4d7322d054316c203f99849939efd5884
veronica <v.escobar1@uniandes.edu.co>
2022-09-14 15:28:52 -0500

    Fixed missing module reference for table

--- src/table/table.module.ts
+++ src/table/table.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TableEntity } from './table.entity';
 import { TableService } from './table.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([TableEntity])],
   providers: [TableService]
 })
 export class TableModule {}
