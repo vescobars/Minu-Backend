877f4a2132f3bdc6aa597cde731b9ed793e9a4ff
nicolas <sn.rodriguez@uniandes.edu.co>
2022-09-14 21:37:50 -0500

    Add initial test configuration

--- src/menu-visual-preferences/menu-visual-preferences.service.spec.ts
+++ src/menu-visual-preferences/menu-visual-preferences.service.spec.ts
@@ -1,18 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { TypeOrmTestingConfig } from 'src/shared/testing-utils/typeorm-testing-config';
+import { Repository } from 'typeorm';
+import { MenuVisualPreferenceEntity } from './menu-visual-preferences.entity';
 import { MenuVisualPreferencesService } from './menu-visual-preferences.service';
 
 describe('MenuVisualPreferencesService', () => {
   let service: MenuVisualPreferencesService;
-
+  let repository: Repository<MenuVisualPreferenceEntity>;
+ 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [...TypeOrmTestingConfig()],
       providers: [MenuVisualPreferencesService],
     }).compile();
-
+ 
     service = module.get<MenuVisualPreferencesService>(MenuVisualPreferencesService);
+    repository = module.get<Repository<MenuVisualPreferenceEntity>>(
+      getRepositoryToken(MenuVisualPreferenceEntity),
+      );
   });
-
+   
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
-});
+ });
--- src/restaurant-chain/restaurant-chain.service.spec.ts
+++ src/restaurant-chain/restaurant-chain.service.spec.ts
@@ -1,18 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { TypeOrmTestingConfig } from 'src/shared/testing-utils/typeorm-testing-config';
+import { Repository } from 'typeorm';
+import { RestaurantChainEntity } from './restaurant-chain.entity';
 import { RestaurantChainService } from './restaurant-chain.service';
 
 describe('RestaurantChainService', () => {
   let service: RestaurantChainService;
-
+  let repository: Repository<RestaurantChainEntity>;
+ 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [...TypeOrmTestingConfig()],
       providers: [RestaurantChainService],
     }).compile();
-
+ 
     service = module.get<RestaurantChainService>(RestaurantChainService);
+    repository = module.get<Repository<RestaurantChainEntity>>(
+      getRepositoryToken(RestaurantChainEntity),
+      );
   });
-
+   
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
-});
+ });
