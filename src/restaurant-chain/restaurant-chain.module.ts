bca9efb5a377319ccbd7586c0aae95e2d23924bb
veronica <v.escobar1@uniandes.edu.co>
2022-09-20 09:48:32 -0500

    Volviendo a hacer lo de controladores

--- src/restaurant-chain/restaurant-chain.module.ts
+++ src/restaurant-chain/restaurant-chain.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantChainEntity } from './restaurant-chain.entity';
 import { RestaurantChainService } from './restaurant-chain.service';
+import { RestaurantChainController } from './restaurant-chain.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([RestaurantChainEntity])],
-  providers: [RestaurantChainService]
+  providers: [RestaurantChainService],
+  controllers: [RestaurantChainController]
 })
 export class RestaurantChainModule {}
--- src/restaurant-site/restaurant-site.module.ts
+++ src/restaurant-site/restaurant-site.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantSiteEntity } from './restaurant-site.entity';
 import { RestaurantSiteService } from './restaurant-site.service';
+import { RestaurantSiteController } from './restaurant-site.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([RestaurantSiteEntity])],
-  providers: [RestaurantSiteService]
+  providers: [RestaurantSiteService],
+  controllers: [RestaurantSiteController]
 })
 export class RestaurantSiteModule {}
