ae95c06f76949e952594e56c30b51ce3c3419835
veronica <v.escobar1@uniandes.edu.co>
2022-09-20 09:57:31 -0500

    Created dto file for restaurant-chain

--- src/restaurant-chain/restaurant-chain.controller.ts
+++ src/restaurant-chain/restaurant-chain.controller.ts
@@ -1,8 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseInterceptors } from '@nestjs/common';
+import { BusinessErrorsInterceptor } from '../shared/interceptors/business-errors.interceptor';
 import { RestaurantChainService } from './restaurant-chain.service';
 
 @Controller('chains')
+@UseInterceptors(BusinessErrorsInterceptor)
 export class RestaurantChainController {
     constructor(private readonly chainService:RestaurantChainService){}
-    
+
 }
