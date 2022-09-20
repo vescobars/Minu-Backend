deba16539822ac9d1c74e1214473acf90f20561d
veronica <v.escobar1@uniandes.edu.co>
2022-09-20 10:02:12 -0500

    Installed clas validator and activated in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { VersioningType } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -10,6 +10,7 @@ async function bootstrap() {
     defaultVersion: '1',
   });
 
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
