597150867a6cb1b694dd4914431f14fa9caff846
veronica <v.escobar1@uniandes.edu.co>
2022-09-20 00:13:37 -0500

    Finished service for order pay mode

--- src/order-pay-mode/order-pay-mode.module.ts
+++ src/order-pay-mode/order-pay-mode.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { OrderEntity } from '../order/order.entity';
+import { OrderService } from '../order/order.service';
 import { OrderPayModeService } from './order-pay-mode.service';
 
 @Module({
-  providers: [OrderPayModeService]
+  imports: [TypeOrmModule.forFeature([OrderEntity])],
+  providers: [OrderService]
 })
 export class OrderPayModeModule {}
--- src/order-pay-mode/order-pay-mode.service.ts
+++ src/order-pay-mode/order-pay-mode.service.ts
@@ -1,4 +1,87 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { OrderEntity } from '../order/order.entity';
+import { PayModeEntity } from '../pay-mode/pay-mode.entity';
+import { Repository } from 'typeorm';
+import { BusinessLogicException, BusinessError } from '../shared/business-errors';
 
 @Injectable()
-export class OrderPayModeService {}
+export class OrderPayModeService {
+    constructor(
+        @InjectRepository(OrderEntity)
+        private readonly orderRepository: Repository<OrderEntity>,
+
+        @InjectRepository(PayModeEntity)
+        private readonly payModeRepository: Repository<PayModeEntity>
+    ){}
+
+    async addPayModeOrder(orderId: string, payModeId: string): Promise<OrderEntity> {
+        const payMode: PayModeEntity = await this.payModeRepository.findOne({where: {id: payModeId}});
+        if (!payMode)
+          throw new BusinessLogicException("The payMode with the given id was not found", BusinessError.NOT_FOUND);
+      
+        const order: OrderEntity = await this.orderRepository.findOne({where: {id: orderId}, relations: ["orderDetail","payMode","table"]})
+        if (!order)
+          throw new BusinessLogicException("The order with the given id was not found", BusinessError.NOT_FOUND);
+    
+        order.payMode = payMode;
+        return await this.orderRepository.save(order);
+    }
+
+    async findPayModeByOrderIdPayModeId(orderId: string, payModeId: string): Promise<PayModeEntity> {
+        const payMode: PayModeEntity = await this.payModeRepository.findOne({where: {id: payModeId}});
+        if (!payMode)
+          throw new BusinessLogicException("The payMode with the given id was not found", BusinessError.NOT_FOUND)
+       
+        const order: OrderEntity = await this.orderRepository.findOne({where: {id: orderId}, relations: ["orderDetail","payMode","table"]});
+        if (!order)
+          throw new BusinessLogicException("The order with the given id was not found", BusinessError.NOT_FOUND)
+   
+        const orderPayMode: PayModeEntity = order.payMode;
+   
+        if (!orderPayMode)
+          throw new BusinessLogicException("The payMode with the given id is not associated to the order", BusinessError.PRECONDITION_FAILED)
+   
+        return orderPayMode;
+    }
+
+    async findPayModeByOrderId(orderId: string): Promise<PayModeEntity> {
+        const order: OrderEntity = await this.orderRepository.findOne({where: {id: orderId}, relations: ["orderDetail","payMode","table"]});
+        if (!order)
+          throw new BusinessLogicException("The order with the given id was not found", BusinessError.NOT_FOUND)
+       
+        return order.payMode;
+    }
+
+    async associatePayModeOrder(orderId: string, payMode: PayModeEntity): Promise<OrderEntity> {
+        const order: OrderEntity = await this.orderRepository.findOne({where: {id: orderId}, relations: ["orderDetail","payMode","table"]});
+    
+        if (!order)
+          throw new BusinessLogicException("The order with the given id was not found", BusinessError.NOT_FOUND)
+    
+        const payModeFound: PayModeEntity = await this.payModeRepository.findOne({where: {id: payMode.id}});
+        if (!payModeFound)
+            throw new BusinessLogicException("The payMode with the given id was not found", BusinessError.NOT_FOUND)
+        
+        order.payMode = payMode;
+        return await this.orderRepository.save(order);
+    }
+
+    async deletePayModeOrder(orderId: string, payModeId: string){
+        const payMode: PayModeEntity = await this.payModeRepository.findOne({where: {id: payModeId}});
+        if (!payMode)
+          throw new BusinessLogicException("The payMode with the given id was not found", BusinessError.NOT_FOUND)
+    
+        const order: OrderEntity = await this.orderRepository.findOne({where: {id: orderId}, relations: ["orderDetail","payMode","table"]});
+        if (!order)
+          throw new BusinessLogicException("The order with the given id was not found", BusinessError.NOT_FOUND)
+    
+        const orderPayMode: PayModeEntity = order.payMode;
+    
+        if (!orderPayMode)
+            throw new BusinessLogicException("The payMode with the given id is not associated to the order", BusinessError.PRECONDITION_FAILED)
+ 
+        order.payMode = null;
+        await this.orderRepository.save(order);
+    }
+}
