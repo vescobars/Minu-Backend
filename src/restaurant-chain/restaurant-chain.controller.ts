9b0d3fd4a39f977348073be3b0c4f738cc3683e1
veronica <v.escobar1@uniandes.edu.co>
2022-09-20 10:09:57 -0500

    Implementacion base de controller para restaurant chain (solo tiene el CRUD para el primer nivel de restaurant chain)

--- src/restaurant-chain/restaurant-chain.controller.ts
+++ src/restaurant-chain/restaurant-chain.controller.ts
@@ -1,5 +1,8 @@
-import { Controller, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put, UseInterceptors } from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
 import { BusinessErrorsInterceptor } from '../shared/interceptors/business-errors.interceptor';
+import { RestaurantChainDto } from './restaurant-chain.dto.ts';
+import { RestaurantChainEntity } from './restaurant-chain.entity';
 import { RestaurantChainService } from './restaurant-chain.service';
 
 @Controller('chains')
@@ -7,4 +10,31 @@ import { RestaurantChainService } from './restaurant-chain.service';
 export class RestaurantChainController {
     constructor(private readonly chainService:RestaurantChainService){}
 
+    @Get()
+    async findAll() {
+        return await this.chainService.findAll();
+    }
+
+    @Get(':chainId')
+    async findOne(@Param('chainId') chainId: string) {
+        return await this.chainService.findOne(chainId);
+    }
+
+    @Post()
+    async create(@Body() chainDto: RestaurantChainDto) {
+        const chain: RestaurantChainEntity = plainToInstance(RestaurantChainEntity, chainDto);
+        return await this.chainService.create(chain);
+    }
+
+    @Put(':chainId')
+        async update(@Param('chainId') chainId: string, @Body() chainDto: RestaurantChainDto) {
+        const chain: RestaurantChainEntity = plainToInstance(RestaurantChainEntity, chainDto);
+        return await this.chainService.update(chainId, chain);
+    }
+
+    @Delete(':chainId')
+    @HttpCode(204)
+    async delete(@Param('chainId') chainId: string) {
+        return await this.chainService.delete(chainId);
+    }
 }
--- src/restaurant-chain/restaurant-chain.dto.ts.ts
+++ src/restaurant-chain/restaurant-chain.dto.ts.ts
@@ -1,6 +1,6 @@
 import {IsNotEmpty, IsString, IsUrl} from 'class-validator';
 
-export class RestaurantChainDtoTs {
+export class RestaurantChainDto{
     @IsString()
     @IsNotEmpty()
     readonly chainName: string;
