5a21073bab6ca3c9ea94454afb4ce44aaf31111d
veronica <v.escobar1@uniandes.edu.co>
2022-09-20 10:15:05 -0500

    Finished client Dto

--- src/client/client.dto.ts
+++ src/client/client.dto.ts
@@ -1,1 +1,18 @@
-export class ClientDto {}
+import {IsNotEmpty, IsString, IsUrl} from 'class-validator';
+export class ClientDto {
+    @IsString()
+    @IsNotEmpty()
+    firstName: string;
+
+    @IsString()
+    @IsNotEmpty()
+    lastName: string;
+
+    @IsString()
+    @IsNotEmpty()
+    phone: string;
+
+    @IsString()
+    @IsNotEmpty()
+    email: string;
+}
