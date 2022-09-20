1cbaa1ea34041bfd4fa8e8c0be1b17de6d78892b
veronica <v.escobar1@uniandes.edu.co>
2022-09-20 10:12:04 -0500

    Created client controler

--- src/client/client.module.ts
+++ src/client/client.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ClientEntity } from './client.entity';
 import { ClientService } from './client.service';
 import { ClientController } from './client.controller';
+import { ClientController } from './client.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ClientEntity])],
