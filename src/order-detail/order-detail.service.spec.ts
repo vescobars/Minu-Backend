233627d8a86283192a2bfa7df7ed23601d90765d
veronica <v.escobar1@uniandes.edu.co>
2022-09-19 20:39:20 -0500

    All tests in order detail except create

--- src/order-detail/order-detail.service.spec.ts
+++ src/order-detail/order-detail.service.spec.ts
@@ -46,6 +46,52 @@ it('findAll should return all orderDetail', async () => {
   expect(orderDetail).toHaveLength(orderDetailList.length);
 });
 
+it('findOne should return a orderDetail by id', async () => {
+  const storedOrderDetail: OrderDetailEntity = orderDetailList[0];
+  const orderDetail: OrderDetailEntity = await service.findOne(storedOrderDetail.id);
+  expect(orderDetail).not.toBeNull();
+  expect(orderDetail.state).toEqual(storedOrderDetail.state)
+  expect(orderDetail.date).toEqual(storedOrderDetail.date)
+  expect(orderDetail.notes).toEqual(storedOrderDetail.notes)
+});
+
+it('findOne should throw an exception for an invalid orderDetail', async () => {
+  await expect(() => service.findOne("0")).rejects.toHaveProperty("message", "The orderDetail with the given id was not found")
+});
+
+it('update should modify a orderDetail', async () => {
+  const orderDetail: OrderDetailEntity = orderDetailList[0];
+  orderDetail.notes = "New notes";
+  orderDetail.state = "New state";
+  const updatedOrderDetail: OrderDetailEntity = await service.update(orderDetail.id, orderDetail);
+  expect(updatedOrderDetail).not.toBeNull();
+  const storedOrderDetail: OrderDetailEntity = await repository.findOne({ where: { id: orderDetail.id } })
+  expect(storedOrderDetail).not.toBeNull();
+  expect(storedOrderDetail.notes).toEqual(orderDetail.notes)
+  expect(storedOrderDetail.state).toEqual(orderDetail.state)
+});
+
+it('update should throw an exception for an invalid orderDetail', async () => {
+  let orderDetail: OrderDetailEntity = orderDetailList[0];
+  orderDetail = {
+    ...orderDetail, notes: "New notes", state: "New state"
+  }
+  await expect(() => service.update("0", orderDetail)).rejects.toHaveProperty("message", "The orderDetail with the given id was not found")
+});
+
+it('delete should remove a orderDetail', async () => {
+  const orderDetail: OrderDetailEntity = orderDetailList[0];
+  await service.delete(orderDetail.id);
+  const deletedOrderDetail: OrderDetailEntity = await repository.findOne({ where: { id: orderDetail.id } })
+  expect(deletedOrderDetail).toBeNull();
+});
+
+
+it('delete should throw an exception for an invalid orderDetail', async () => {
+  const orderDetail: OrderDetailEntity = orderDetailList[0];
+  await service.delete(orderDetail.id);
+  await expect(() => service.delete("0")).rejects.toHaveProperty("message", "The orderDetail with the given id was not found")
+});
 
 });
 
