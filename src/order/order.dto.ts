20b1d064c0a78134ccb5f41848ceaf96f0f5fa58
veronica <v.escobar1@uniandes.edu.co>
2022-09-21 11:30:38 -0500

    Order DTO

--- src/order/order.dto.ts
+++ src/order/order.dto.ts
@@ -1,1 +1,15 @@
-export class OrderDto {}
+import { IsDate, IsNotEmpty, IsNumber, IsString } from "class-validator";
+
+export class OrderDto {
+    @IsString()
+    @IsNotEmpty()
+    state: string;
+
+    @IsDate()
+    @IsNotEmpty()
+    date: Date;
+
+    @IsNumber()
+    @IsNotEmpty()
+    totalValue: number;
+}
