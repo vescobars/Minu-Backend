9e02cf1d8ecaeec5d0f7d698dc8620ac7d836e41
veronica <v.escobar1@uniandes.edu.co>
2022-09-25 21:17:22 -0500

    Fixing association routes para asociaciones de order

--- src/order-detail-plates/order-detail-plates.module.ts
+++ src/order-detail-plates/order-detail-plates.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { OrderDetailService } from '../order-detail/order-detail.service';
 import { OrderDetailEntity } from '../order-detail/order-detail.entity';
-//import { OrderDetailPlatesController } from './order-detail-plates.controller';
+import { OrderDetailPlatesController } from './order-detail-plates.controller';
+import { PlateEntity } from '../plate/plate.entity';
+import { OrderDetailPlatesService } from './order-detail-plates.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([OrderDetailEntity])],
-  providers: [OrderDetailService],
-  //controllers: [OrderDetailPlatesController]
+  imports: [TypeOrmModule.forFeature([OrderDetailEntity,PlateEntity])],
+  providers: [OrderDetailPlatesService],
+  controllers: [OrderDetailPlatesController]
 })
 export class OrderDetailPlatesModule {}
--- src/order-pay-mode/order-pay-mode.module.ts
+++ src/order-pay-mode/order-pay-mode.module.ts
@@ -4,7 +4,7 @@ import { OrderEntity } from '../order/order.entity';
 import { OrderService } from '../order/order.service';
 import { OrderPayModeService } from './order-pay-mode.service';
 import { OrderPayModeController } from './order-pay-mode.controller';
-import { PayModeEntity } from 'src/pay-mode/pay-mode.entity';
+import { PayModeEntity } from '../pay-mode/pay-mode.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([OrderEntity,PayModeEntity])],
