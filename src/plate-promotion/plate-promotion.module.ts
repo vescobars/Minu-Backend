25453b2ee7a70c8f01e63e8d10b47f04a93c3dc8
Gabriela Cagua <g.cagua@uniandes.edu.co>
2022-09-25 19:03:06 -0500

    merged before

--- src/plate-promotion/plate-promotion.module.ts
+++ src/plate-promotion/plate-promotion.module.ts
@@ -4,9 +4,6 @@ import { PlateEntity } from 'src/plate/plate.entity';
 import { PromotionEntity } from 'src/promotion/promotion.entity';
 import { PlatePromotionService } from './plate-promotion.service';
 import { PlatePromotionController } from './plate-promotion.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { PromotionEntity } from '../promotion/promotion.entity';
-import { PlateEntity } from '../plate/plate.entity';
 
 @Module({
   providers: [PlatePromotionService],
