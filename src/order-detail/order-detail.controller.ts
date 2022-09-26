01880fb0e80e13bbf9b150fb5a06938e2e21435a
veronica <v.escobar1@uniandes.edu.co>
2022-09-25 19:07:33 -0500

    Moded route for OrderDetailController para que fuera mas directa

--- src/order-detail/order-detail.controller.ts
+++ src/order-detail/order-detail.controller.ts
@@ -5,34 +5,34 @@ import { OrderDetailDto } from './order-detail.dto';
 import { OrderDetailEntity } from './order-detail.entity';
 import { OrderDetailService } from './order-detail.service';
 
-@Controller('chains')
+@Controller('orderDetail')
 @UseInterceptors(BusinessErrorsInterceptor)
 export class OrderDetailController {
     constructor(private readonly orderDetailService:OrderDetailService){}
     
-    @Get(':chainId/sites/:siteId/orders/:orderId/orderDetails')
+    @Get()
     async findAll() {
         return await this.orderDetailService.findAll();
     }
 
-    @Get(':chainId/sites/:siteId/orders/:orderId/orderDetails/:orderDetailId')
+    @Get(':orderDetailId')
     async findOne(@Param('orderDetailId') orderDetailId: string) {
         return await this.orderDetailService.findOne(orderDetailId);
     }
 
-    @Post(':chainId/sites/:siteId/orders/:orderId/orderDetails')
+    @Post()
     async create(@Body() orderDetailDto: OrderDetailDto) {
         const orderDetail: OrderDetailEntity = plainToInstance(OrderDetailEntity, orderDetailDto);
         return await this.orderDetailService.create(orderDetail);
     }
 
-    @Put(':chainId/sites/:siteId/orders/:orderId/orderDetails/:orderDetailId')
+    @Put(':orderDetailId')
     async update(@Param('orderDetailId') orderDetailId: string, @Body() orderDetailDto: OrderDetailDto) {
         const orderDetail: OrderDetailEntity = plainToInstance(OrderDetailEntity, orderDetailDto);
         return await this.orderDetailService.update(orderDetailId, orderDetail);
     }
 
-    @Delete(':chainId/sites/:siteId/orders/:orderId/orderDetails/:orderDetailId')
+    @Delete(':orderDetailId')
     @HttpCode(204)
     async delete(@Param('orderDetailId') orderDetailId: string) {
         return await this.orderDetailService.delete(orderDetailId);
