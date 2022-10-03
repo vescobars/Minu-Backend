69940961f35dca980da7261fd2be46767ba1516e
Gabriela Cagua <g.cagua@uniandes.edu.co>
2022-10-02 23:52:08 -0500

    adas

--- src/category-plate/category-plate.controller.ts
+++ src/category-plate/category-plate.controller.ts
@@ -5,38 +5,46 @@ import { PlateDto } from '../plate/plate.dto';
 import { PlateEntity } from '../plate/plate.entity';
 import { BusinessErrorsInterceptor } from '../shared/interceptors/business-errors.interceptor';
 import { CategoryPlateService } from './category-plate.service';
+import { HasRoles } from '../shared/security/roles.decorators';
+import { Role } from '../enums/role.enum';
+import { RolesGuard } from '../auth/guards/role.guard';
 
 @Controller('categories')
 @UseInterceptors(BusinessErrorsInterceptor)
 export class CategoryPlateController {
    constructor(private readonly categoryPlateService: CategoryPlateService){}
   
-   @UseGuards(JwtAuthGuard)
+   @HasRoles(Role.Writer)
+   @UseGuards(JwtAuthGuard, RolesGuard)
    @Post(':categoryId/plates/:plateId')
    async addPlateCategory(@Param('categoryId') categoryId: string, @Param('plateId') plateId: string){
        return await this.categoryPlateService.addPlateCategory(categoryId, plateId);
    }
   
-   @UseGuards(JwtAuthGuard)
+   @HasRoles(Role.Reader)
+   @UseGuards(JwtAuthGuard, RolesGuard)
    @Get(':categoryId/plates/:plateId')
    async findPlateByCategoryIdPlateId(@Param('categoryId') categoryId: string, @Param('plateId') plateId: string){
        return await this.categoryPlateService.findPlateByCategoryIdPlateId(categoryId, plateId);
    }
   
-   @UseGuards(JwtAuthGuard)
+   @HasRoles(Role.Reader)
+   @UseGuards(JwtAuthGuard, RolesGuard)
    @Get(':categoryId/plates')
    async findPlatesByCategoryId(@Param('categoryId') categoryId: string){
        return await this.categoryPlateService.findPlatesByCategoryId(categoryId);
    }
   
-   @UseGuards(JwtAuthGuard)
+   @HasRoles(Role.Writer)
+   @UseGuards(JwtAuthGuard, RolesGuard)
    @Put(':categoryId/plates')
    async associatePlatesCategory(@Body() platesDto: PlateDto[], @Param('categoryId') categoryId: string){
        const plates = plainToInstance(PlateEntity, platesDto)
        return await this.categoryPlateService.associatePlatesCategory(categoryId, plates);
    }
   
-   @UseGuards(JwtAuthGuard)
+   @HasRoles(Role.Deleter)
+   @UseGuards(JwtAuthGuard, RolesGuard)
    @Delete(':categoryId/plates/:plateId')
    @HttpCode(204)
    async deletePlateCategory(@Param('categoryId') categoryId: string, @Param('plateId') plateId: string){
--- src/description-tag/description-tag.controller.ts
+++ src/description-tag/description-tag.controller.ts
@@ -5,36 +5,46 @@ import { BusinessErrorsInterceptor } from '../shared/interceptors/business-error
 import { DescriptionTagDto } from './description-tag.dto';
 import { DescriptionTagEntity } from './description-tag.entity';
 import { DescriptionTagService } from './description-tag.service';
+import { HasRoles } from '../shared/security/roles.decorators';
+import { Role } from '../enums/role.enum';
+import { RolesGuard } from '../auth/guards/role.guard';
 
 @Controller('description-tags')
 @UseInterceptors(BusinessErrorsInterceptor)
 export class DescriptionTagController {
     constructor(private readonly descriptionTagService: DescriptionTagService) {}
-  
-    @UseGuards(JwtAuthGuard)
+    @HasRoles(Role.Reader)
+    @UseGuards(JwtAuthGuard, RolesGuard)
     @Get()
     async findAll() {
     return await this.descriptionTagService.findAll();
     }
   
-    @UseGuards(JwtAuthGuard)
+    @HasRoles(Role.Reader)
+    @UseGuards(JwtAuthGuard, RolesGuard)
     @Get(':descriptionTagId')
     async findOne(@Param('descriptionTagId') descriptionTagId: string) {
         return await this.descriptionTagService.findOne(descriptionTagId);
     }
-
+  
+    @HasRoles(Role.Writer)
+    @UseGuards(JwtAuthGuard, RolesGuard)
     @Post()
     async create(@Body() descriptionTagDto: DescriptionTagDto) {
         const descriptionTag: DescriptionTagEntity = plainToInstance(DescriptionTagEntity, descriptionTagDto);
         return await this.descriptionTagService.create(descriptionTag);
     }
-
+  
+    @HasRoles(Role.Writer)
+    @UseGuards(JwtAuthGuard, RolesGuard)
     @Put(':descriptionTagId')
     async update(@Param('descriptionTagId') descriptionTagId: string, @Body() descriptionTagDto: DescriptionTagDto) {
         const descriptionTag: DescriptionTagEntity = plainToInstance(DescriptionTagEntity, descriptionTagDto);
         return await this.descriptionTagService.update(descriptionTagId, descriptionTag);
     }
-
+  
+    @HasRoles(Role.Deleter)
+    @UseGuards(JwtAuthGuard, RolesGuard)
     @Delete(':descriptionTagId')
     @HttpCode(204)
     async delete(@Param('descriptionTagId') descriptionTagId: string) {
